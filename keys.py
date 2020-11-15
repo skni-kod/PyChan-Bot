d1ed0c4f614fc77983ad8b5f6f98a875718dec1e
Kazan1520 <kazunnoto@gmail.com>
2020-11-15 22:36:15 +0100

    Code refactor
    Code refactor

---
+++ keys.py
@@ -0,0 +1,3 @@
+GEOCODER_KEY = '28b71ffee29f45b6bf9ba0712399da51'
+FORECAST_KEY = '6b57d5588d1e88f2c0027c0741f21a68'
+TOKEN = "NTMxOTUzMTY1MDA4ODk2MDQ2.XDPJ_Q.kZrpZMEylErShRkwTjBU1y68sT4"  # Get at discordapp.com/developers/applications/me
