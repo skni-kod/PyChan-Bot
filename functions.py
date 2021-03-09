62fc46609c30a283338a2d5ae78fba997ca6b631
karol2502 <64597076+karol2502@users.noreply.github.com>
2021-03-09 23:53:23 +0100

    Delete functions.py

--- functions.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def getDataFromAttachment(ctx):
-    a = ctx.message.attachments[0]
-    url = a.url
-    file = requests.get(url)
-    soup = BeautifulSoup(file.text, "html.parser")
-    return soup
