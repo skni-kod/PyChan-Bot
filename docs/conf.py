d1fbda963cd1202bf1674b7819e1c03f5c9ce2a1
Karol Angrys <karol.angrys@gmail.com>
2021-04-20 01:07:51 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../PyChan'))
 
+sys.path.insert(0, os.path.abspath('../PyChan'))
 
 # -- Project information -----------------------------------------------------
 
@@ -23,7 +23,6 @@ author = 'SKNI KOD'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.8'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -42,7 +41,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
