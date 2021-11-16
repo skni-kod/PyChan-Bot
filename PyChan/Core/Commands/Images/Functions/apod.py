dcb5f397add0547dd1abaa2abdd65099da475bf5
Dawid Klimkiewicz <82545462+klkkl2@users.noreply.github.com>
2021-11-16 14:59:14 +0100

    Update apod.py

--- PyChan/Core/Commands/Images/Functions/apod.py
+++ PyChan/Core/Commands/Images/Functions/apod.py
@@ -10,10 +10,31 @@ class ApodImage(commands.Cog):
     @commands.command(pass_context=True, name='apod')
     async def apod(self, ctx):
         data = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={apod_token}")
+        #imgurl = data.json()['hdurl']
+        imgurl = "https://www.youtube.com/watch?v=7NykS2kv_k8&list=TLGGNhcYlATJB8kxNjExMjAyMQ&t=4s"
 
         embed = discord.Embed(title='Astronomy picture of the day',
                             description= data.json()['explanation'],
                             color=discord.Color.dark_purple())
 
-        embed.set_image(url = data.json()['hdurl'])
+        if("youtube.com" in imgurl):
+            #await ctx.send(imgurl)
+            TestVideoEmbed = {}
+            TestVideoEmbed['type'] = 'video'
+            TestVideoEmbed['url'] = 'https://www.youtube.com/watch?v=7NykS2kv_k8&list=TLGGNhcYlATJB8kxNjExMjAyMQ&t=4s'
+            VideoDict = {}
+            VideoDict['height'] = 480
+            VideoDict['proxy_url'] = 'https://www.youtube.com/watch?v=7NykS2kv_k8&list=TLGGNhcYlATJB8kxNjExMjAyMQ&t=4s'
+            VideoDict['url'] = 'https://www.youtube.com/watch?v=7NykS2kv_k8&list=TLGGNhcYlATJB8kxNjExMjAyMQ&t=4s'
+            VideoDict['width'] = 480
+            TestVideoEmbed['video'] = VideoDict
+            CreatedEmbed = discord.embeds.Embed.from_dict(TestVideoEmbed)
+            await ctx.send(embed = CreatedEmbed)
+        else:
+            embed.set_image(url = imgurl)
+
         await ctx.send(embed = embed)
+
+
+
+        
