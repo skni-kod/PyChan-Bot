a1dbac47dd7ce37b6ef6ad527dab48b6b2629363
Karol Angrys <karol.angrys@gmail.com>
2021-11-20 23:02:16 +0100

    help update

--- PyChan/Core/Commands/Help/help.py
+++ PyChan/Core/Commands/Help/help.py
@@ -27,20 +27,18 @@ class Help(commands.Cog):
                                           '\n'
                                           'Dostępne komendy:',
                               color=discord.Color.dark_purple())
-        embed.add_field(name='Matematyka',
+        embed.add_field(name='Nauka',
                         value='`zamiana`, `zamiana+`, `ieee754_32`, `ieee754_64`, `permutacje`, `booth`',
                         inline=False)
         embed.add_field(name='Obraz',
                         value='`ocr`, `apod`, `qr`',
                         inline=False)
-        embed.add_field(name='SKNIKOD',
-                        value='`listaCzlonkow`',
-                        inline=False)
         embed.add_field(name='Tekst',
                         value='`ciekawostka`',
                         inline=False)
-        
-
+        embed.add_field(name='SKNIKOD',
+                        value='`listaCzlonkow`',
+                        inline=False)
         await ctx.send(embed=embed)
 
     @help.command(name='zamiana')
@@ -146,11 +144,8 @@ class Help(commands.Cog):
         await ctx.send(embed=embed)
 
     @help.command(name='ciekawostka')
-    async def facts_help(self, ctx):
+    async def fun_fact_help(self, ctx):
         embed = discord.Embed(title='ciekawostka',
                               description='Wysyła losową ciekawostkę',
                               color=discord.Color.dark_purple())
         await ctx.send(embed=embed)
-
-
-    
