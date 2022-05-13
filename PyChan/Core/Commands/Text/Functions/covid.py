441233db984d5184864eb1d63aa105502b148a0d
Ernest Bytnar <ernestbytnar@gmail.com>
2022-05-13 13:48:29 +0200

    covid
    dopisanie helpa

--- PyChan/Core/Commands/Text/Functions/covid.py
+++ PyChan/Core/Commands/Text/Functions/covid.py
@@ -1,23 +1,30 @@
 import discord
 from discord.ext import commands
 import requests
+from Core.Decorators.decorators import Decorator
 
 
 
 class Covid(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command(pass_context=True, name='covid')  
+        
+    @commands.command(
+        pass_context=True,
+        name="covid",
+        category="Tekst",
+        help_={
+            "title": "covid",
+            "description": "Liczba zachorowań na covid w Polsce z wczoraj ",
+           
+        },
+    )
+    @Decorator.pychan_decorator
     async def covid_zakazenia(self,ctx):
-
-    
 
         url = "https://api.covid19api.com/live/country/poland/status/confirmed"
         response = requests.get(url,verify = False)
         api_data = response.json()
-
-
 
         liczba_zachorowan = api_data[::-1][0]['Confirmed'] - api_data[::-1][1]['Confirmed']
         dzien = api_data[::-1][1]["Date"]
