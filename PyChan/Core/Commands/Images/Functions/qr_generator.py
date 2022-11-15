e9d60765030f715ac518f1bc0c1230ed0fd8cd49
Michał Partyka <h3ryin@gmail.com>
2022-11-15 13:47:07 +0100

    Update help for qr_generator

--- PyChan/Core/Commands/Images/Functions/qr_generator.py
+++ PyChan/Core/Commands/Images/Functions/qr_generator.py
@@ -9,7 +9,15 @@ class QR(commands.Cog):
         """Constructor method"""
         self.bot = bot
 
-    @commands.command(pass_context=True, name="qr", category="Obraz")
+    @commands.command(
+        pass_context=True, 
+        name="qr", 
+        category="Obraz",
+        usage = "<link do obrazu>",
+        help = """
+               Generuje kod QR z podanego linku do obrazu.
+               """
+        )
     async def qr(self, ctx, arg):
         embed = nextcord.Embed()
         embed.set_image(url="https://www.qrtag.net/api/qr_6.png?url=" + str(arg))
