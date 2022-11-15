d6a59f6a9f6159f1d0c24533728f96b5adf64514
Michał Partyka <h3ryin@gmail.com>
2022-11-15 13:41:14 +0100

    Update help for fun_fact

--- PyChan/Core/Commands/Text/Functions/fun_fact.py
+++ PyChan/Core/Commands/Text/Functions/fun_fact.py
@@ -7,7 +7,15 @@ class FunFact(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, name="ciekawostka", category="Tekst")
+    @commands.command(
+        pass_context = True, 
+        name = "ciekawostka", 
+        category = "Tekst",
+        usage = " ",
+        help = """
+               Wysyła losową ciekawostkę w języku angielskim.
+               """
+    )
     async def fun_fact(self, ctx):
         r = requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
         r = r.json()
