55d52d94319c0008f9763faf164cef9ecadb2302
Michał Partyka <h3ryin@gmail.com>
2022-11-15 13:44:35 +0100

    Moved qr to Images

--- PyChan/Core/Commands/Images/images.py
+++ PyChan/Core/Commands/Images/images.py
@@ -2,6 +2,7 @@ import nextcord
 from nextcord.ext import commands
 
 from Core.Commands.Images.Functions.ocr import OCR
+from Core.Commands.Images.Functions.qr_generator import QR
 from Core.Commands.Images.Functions.apod import ApodImage
 from Core.Commands.Images.Functions.waifu2x import Waifu2x
 
@@ -15,5 +16,6 @@ class Images(commands.Cog):
         """
         self.bot = bot
         self.bot.add_cog(OCR(bot))
+        self.bot.add_cog(QR(bot))
         self.bot.add_cog(ApodImage(bot))
         self.bot.add_cog(Waifu2x(bot))
--- PyChan/Core/Commands/Utilities/utilities.py
+++ PyChan/Core/Commands/Utilities/utilities.py
@@ -2,7 +2,6 @@ import nextcord
 from nextcord.ext import commands
 
 from Core.Commands.Utilities.Functions.ssh import SSH
-from Core.Commands.Utilities.Functions.qr_generator import QR
 from Core.Commands.Utilities.Functions.AoC import AoC
 from Core.Commands.Utilities.Functions.west import KanyeWestQuete
 
@@ -15,6 +14,5 @@ class Utilities(commands.Cog):
         """
         self.bot = bot
         self.bot.add_cog(SSH(bot))
-        self.bot.add_cog(QR(bot))
         self.bot.add_cog(AoC(bot))
         self.bot.add_cog(KanyeWestQuete(bot))
