58f8d6f380476076af81681432e9c39919b851fd
Dziaduch <48856951+n0tcnj1eck4h@users.noreply.github.com>
2022-12-07 17:58:11 +0100

    Revert listaCzlonkow

--- PyChan/Core/Commands/SKNIKOD/Functions/get_members_projects.py
+++ PyChan/Core/Commands/SKNIKOD/Functions/get_members_projects.py
@@ -1,6 +1,6 @@
-from tempfile import NamedTemporaryFile 
 import nextcord
 from nextcord.ext import commands
+import os
 
 
 class GetMembersProjects(commands.Cog):
@@ -16,7 +16,7 @@ class GetMembersProjects(commands.Cog):
                Wysyła plik txt z aktualną listą członków z rolą `Członek` i przypisanymi do nich projektami
                """
     )
-    async def get_members_projects(self, ctx: commands.Context):
+    async def get_members_projects(self, ctx):
         """
         Gets actual list of members and send txt file with name and projects assigned to them
 
@@ -24,11 +24,16 @@ class GetMembersProjects(commands.Cog):
         :type ctx: nextcord.ext.commands.Context
         """
 
-        message = await ctx.send("Trwa pobieranie czlonkow...")
-        file = NamedTemporaryFile(mode="r+")
+        message = await ctx.send(
+            f"Trwa pobieranie czlonkow {1}/{len(ctx.guild.members)}"
+        )
 
-        async with ctx.channel.typing():
-            for member in ctx.guild.members:
+        with open("listaCzlonkow.txt", "w", encoding="utf-8") as f:
+            for i, member in enumerate(ctx.guild.members):
+                await message.edit(
+                    content=f"Trwa pobieranie członkow {i}/{len(ctx.guild.members)}"
+                )
+
                 if member.bot:
                     continue
 
@@ -37,19 +42,21 @@ class GetMembersProjects(commands.Cog):
                         name = member.display_name.replace('"', " ").split()
 
                         if len(name) > 2:
-                            file.write(f"{name[0]};{name[2]};{name[1]};")
+                            f.write(f"{name[0]};{name[2]};{name[1]};")
                         elif len(name) == 2:
-                            file.write(f"{name[0]};{name[1]};;")
+                            f.write(f"{name[0]};{name[1]};;")
                         else:
-                            file.write(f"{member.display_name} error")
+                            f.write(f"{member.display_name} error")
 
                         for role2 in member.roles[1:]:
                             if "Projekt" in role2.name:
                                 str = role2.name.replace("Projekt - ", "")
-                                file.write(f"{str}, ")
+                                f.write(f"{str}, ")
 
-                        file.write("\n")
+                        f.write("\n")
 
         await message.delete()
-        await ctx.send(file=nextcord.File(file.file, "listaCzlonkow.txt", force_close=True))
+        with open("listaCzlonkow.txt", "rb") as file:
+            await ctx.send(file=nextcord.File(file, "listaCzlonkow.txt"))
 
+        os.remove("listaCzlonkow.txt")
