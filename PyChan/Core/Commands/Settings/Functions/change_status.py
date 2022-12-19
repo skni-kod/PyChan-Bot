5e794b0ecf4680bf6853adcfb3f31815b138833c
Dziaduch <48856951+n0tcnj1eck4h@users.noreply.github.com>
2022-12-19 11:13:20 +0100

    Fix status

--- PyChan/Core/Commands/Settings/Functions/change_status.py
+++ PyChan/Core/Commands/Settings/Functions/change_status.py
@@ -2,15 +2,17 @@ import nextcord
 from nextcord.ext import commands, tasks
 from itertools import cycle
 
+import config
+
 class ChangeStatus(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
-    self.status = cycle([nextcord.Activity(type=nextcord.ActivityType.watching, name='^help and eaten snake'),
+    self.status = cycle([nextcord.Activity(type=nextcord.ActivityType.watching, name=f'{config.default_prefix}help'),
                         nextcord.Game('Snake in Snake')])
 
-  @tasks.loop(minutes=30)
-  async def change_status(self):
-      game = nextcord.Game("with the API")
-      await self.bot.change_presence(activity=next(self.status))
+  # @tasks.loop(minutes=30)
+  # async def change_status(self):
+  #     game = nextcord.Game("with the API")
+  #     await self.bot.change_presence(activity=next(self.status))
 
 
