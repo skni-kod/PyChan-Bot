0c1f8bd06649520496c28b2c65dc4765766bc9bf
Dziaduch <48856951+n0tcnj1eck4h@users.noreply.github.com>
2023-02-02 12:17:04 +0100

    Mark some commands as administrator only and add a handler for MissingPermissions exception

--- pychan/errors.py
+++ pychan/errors.py
@@ -1,5 +1,5 @@
 from nextcord import Color, Embed
-from nextcord.ext.commands import BadArgument, Cog, CommandError, CommandNotFound, Context, MissingRequiredArgument
+from nextcord.ext.commands import BadArgument, Cog, CommandError, CommandNotFound, Context, MissingPermissions, MissingRequiredArgument
 
 
 class Errors(Cog):
@@ -14,12 +14,22 @@ class Errors(Cog):
                             value='Podana komenda nie istnieje',
                             inline=False)
             await ctx.send(embed=embed)
+
         elif isinstance(error, BadArgument):
             await ctx.send('Niepoprawny parametr')
+
         elif isinstance(error, MissingRequiredArgument):
             await ctx.send('Brakuje wymaganego parametru')
+
+        elif isinstance(error, MissingPermissions):
+            embed = Embed(color=Color.dark_purple())
+            embed.add_field(name='Brak uprawnień',
+                            value='Brakujące uprawinienia to: `' + ', '.join(error.missing_permissions) + '`',
+                            inline=False)
+            await ctx.send(embed=embed)
+
         elif isinstance(error, Exception):
-            await ctx.send('Coś poszło nie tak')
+            await ctx.send(f'Coś poszło nie tak (`{type(error).__name__}`)')
             print(error)
         else:
             print(error)
