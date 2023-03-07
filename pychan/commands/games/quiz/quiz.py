5c41e5379bc9141cc0bff9378335b188a4e0c7de
kubabartecki <169511@stud.prz.edu.pl>
2023-03-07 23:30:50 +0100

    display bug fix

--- pychan/commands/games/quiz/quiz.py
+++ pychan/commands/games/quiz/quiz.py
@@ -1,6 +1,7 @@
 from nextcord.ext import commands
 from nextcord import Colour, Embed
 import nextcord
+from asyncio import sleep
 
 from pychan import database
 from .modals import EmbedModal
@@ -10,34 +11,27 @@ class Quiz(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name = "quiz", category = "Gry", pass_context = True)
+    @commands.group(name = "quiz", category = "Gry", pass_context = True)
     async def quiz(self, ctx: commands.Context):
-        '''Wyswietla menu quizu'''
+        '''Komendy do quizu'''
         
+
+    @quiz.command(name="start", pass_context = True)
+    async def start(self, ctx):
+        '''Rozpoczyna quiz'''
+
         embed = Embed(
-                title = f"Rozpocznij quiz, dodaj pytanie do bazy lub wyświetl ranking",
-                color = Colour.green(), 
-            )
-
+                    title = f"Rozpoczynamy za ... 3",
+                    color = Colour.green(),
+                )
         viewYouCanEdit = await ctx.send(embed=embed)
-        starter = MenuButtons(viewYouCanEdit)
-        await viewYouCanEdit.edit(view=starter)
+        await sleep(1)
 
-        await starter.wait()
+        for i in [2, 1]:
+            embed.title = f"Rozpoczynamy za ... {i}"
+            await viewYouCanEdit.edit(embed = embed)
+            await sleep(1)
         
-
-class MenuButtons(nextcord.ui.View):
-    def __init__(self, viewYouCanEdit: nextcord.Message):
-        super().__init__()
-        self.value = None
-        self.viewYouCanEdit = viewYouCanEdit
-        
-
-    #start
-    @nextcord.ui.button(label = "Start", style=nextcord.ButtonStyle.green)
-    async def startB(self, bbutton: nextcord.ui.Button, interaction: nextcord.Interaction):
-        self.value = True
-
         points = [0]
         all_questions: list[database.QuizQuestion] = database.session.query(database.QuizQuestion).all()
         quesitions = all_questions[:5]
@@ -48,13 +42,34 @@ class MenuButtons(nextcord.ui.View):
                 button = AnswerButton(ans, points)
                 quizView.add_item(button)
 
-            await self.viewYouCanEdit.edit(view=quizView, embed=quizView.embed)
+            await viewYouCanEdit.edit(view=quizView, embed=quizView.embed)
             await quizView.wait()
             print(points[0],"koncowe")
 
         quiz_summary = Embed(title=f"Gratulacje, ilość punktów to: {points[0]}")
-        await self.viewYouCanEdit.edit(view=None, embed=quiz_summary)
-        self.stop()
+        await viewYouCanEdit.edit(view=None, embed=quiz_summary)
+        
+    
+    @quiz.command(name="menu", pass_context = True)
+    async def menu(self, ctx):
+        '''Dodawanie pytan, ranking'''
+
+        embed = Embed(
+                title = f"Dodaj pytanie do bazy lub wyświetl ranking",
+                color = Colour.green(), 
+            )
+
+        viewYouCanEdit = await ctx.send(embed=embed)
+        starter = MenuButtons(viewYouCanEdit)
+        await viewYouCanEdit.edit(view=starter)
+
+        await starter.wait()
+
+class MenuButtons(nextcord.ui.View):
+    def __init__(self, viewYouCanEdit: nextcord.Message):
+        super().__init__()
+        self.value = None
+        self.viewYouCanEdit = viewYouCanEdit
 
     #add
     @nextcord.ui.button(label = "Dodaj", style=nextcord.ButtonStyle.blurple)
@@ -64,13 +79,13 @@ class MenuButtons(nextcord.ui.View):
         mod = EmbedModal(self.viewYouCanEdit)
         await interaction.response.send_modal(mod)
 
-        #self.stop()
+        self.stop()
 
     #ranking
     @nextcord.ui.button(label = "Ranking", style=nextcord.ButtonStyle.red)
     async def rankingB(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
         self.value = True
-        #self.stop()
+        self.stop()
 
 class AnswerButton(nextcord.ui.Button):
     def __init__(self, answer: database.QuizAnswer, points: list[int]):
@@ -87,11 +102,4 @@ class AnswerButton(nextcord.ui.Button):
             self.points[0] += 1
         # moze tak lepiej self.view.points += 1
         self.view.stop()
-        
-
-
-    '''second modal
-    unused, but maybe someone will find a way to apply 2 modals...
-    and StringSelect will be supported in Modals
-    '''
  
