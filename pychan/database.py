8c636cb4d9a0fee79b9cd125959c98c7860fd6cb
Michał Partyka <h3ryin@gmail.com>
2024-02-27 08:50:50 +0100

    Rewrite database file

--- pychan/database.py
+++ pychan/database.py
@@ -2,61 +2,51 @@ from typing import Literal, Optional, Union
 from nextcord import Guild, Member, User
 from nextcord.ext.commands.bot import Bot
 from nextcord.message import Message
-from sqlalchemy import ForeignKey, create_engine, insert, select, update
-from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column, relationship, sessionmaker
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, select, insert, update, create_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 import config
 
 engine = create_engine(config.sqlalchemy_db_url, echo=config.database_echo)
-
-def session() -> Session:
-    return Session(engine)
-
-
-class Base(DeclarativeBase):
-    pass
-
-# Stats and settings for guild members
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
 
 
 class GuildMember(Base):
     __tablename__ = 'guild_members'
-    member_id: Mapped[int] = mapped_column(primary_key=True)
-    guild_id: Mapped[int] = mapped_column()
-    coins: Mapped[int] = mapped_column(default=0)
-
-# Stats and settings for users across all guilds
+    member_id = Column(String, primary_key=True)
+    guild_id = Column(String)
+    coins = Column(Integer, default=0)
 
 
 class DiscordUser(Base):
     __tablename__ = 'discord_users'
-    user_id: Mapped[int] = mapped_column(primary_key=True)
-    osu_username: Mapped[Optional[str]] = mapped_column()
-    osrs_username: Mapped[Optional[str]] = mapped_column()
-    lol_username: Mapped[Optional[str]] = mapped_column()
+    user_id = Column(String, primary_key=True)
+    osu_username = Column(String)
+    osrs_username = Column(String)
+    lol_username = Column(String)
 
 
 class GuildSettings(Base):
     __tablename__ = 'guild_settings'
-    guild_id: Mapped[int] = mapped_column(primary_key=True)
-    prefix: Mapped[str] = mapped_column(default=config.default_prefix)
+    guild_id = Column(String, primary_key=True)
+    prefix = Column(String, default=config.default_prefix)
 
 
 class QuizQuestion(Base):
     __tablename__ = 'quiz_questions'
-    question_id: Mapped[int] = mapped_column(primary_key=True)
-    answers: Mapped[list["QuizAnswer"]] = relationship()
-    guild_id: Mapped[int] = mapped_column()
-    question: Mapped[str] = mapped_column()
-    category: Mapped[str] = mapped_column()
+    question_id = Column(Integer, primary_key=True)
+    guild_id = Column(String)
+    question = Column(String)
+    category = Column(String)
+    answers = relationship("QuizAnswer")
 
 
 class QuizAnswer(Base):
     __tablename__ = 'quiz_answers'
-    answer_id: Mapped[int] = mapped_column(primary_key=True)
-    question_id: Mapped[int] = mapped_column(
-        ForeignKey('quiz_questions.question_id'))
-    answer: Mapped[str] = mapped_column()
-    correct: Mapped[bool] = mapped_column()
+    answer_id = Column(Integer, primary_key=True)
+    question_id = Column(Integer, ForeignKey('quiz_questions.question_id'))
+    answer = Column(String)
+    correct = Column(Boolean)
 
 
 def create_database():
@@ -66,12 +56,12 @@ def create_database():
 def get_guild_prefix(_: Bot, message: Message) -> str:
     if not message.guild:
         return ''
-    with session() as s:
+    with Session() as s:
         return s.scalar(select(GuildSettings.prefix).where(GuildSettings.guild_id == message.guild.id)) or config.default_prefix
 
 
 def set_guild_prefix(guild: Guild, prefix: str):
-    with session() as s:
+    with Session() as s:
         tag = s.scalar(
             select(GuildSettings.prefix)
             .where(GuildSettings.guild_id == guild.id))
@@ -89,7 +79,7 @@ def set_guild_prefix(guild: Guild, prefix: str):
 
 
 def set_game_username(member: Union[User, Member], username: str, game: Literal['osu', 'lol', 'osrs']):
-    with session() as s:
+    with Session() as s:
         select_stmt = select(DiscordUser).where(DiscordUser.user_id == member.id)
         user = s.scalar(select_stmt)
         if not user:
@@ -97,12 +87,12 @@ def set_game_username(member: Union[User, Member], username: str, game: Literal[
             setattr(user, game + '_username', username)
             s.add(user)
         else:
-            user.osu_username = username
+            setattr(user, game + '_username', username)
         s.commit()
 
 
 def get_game_username(member: Union[User, Member], game: Literal['osu', 'lol', 'osrs']) -> Optional[str]:
-    with session() as s:
+    with Session() as s:
         select_stmt = select(DiscordUser).where(DiscordUser.user_id == member.id)
         user = s.scalar(select_stmt)
         if not user:
